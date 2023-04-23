b82a10245b54b49ca51cf16a3dca4695c23137e3
ngtienndungg <dungnt.works@gmail.com>
2023-04-23 11:50:01 +0700

    adjust folder structures

--- UI/FormDangNhap.cs
+++ UI/FormDangNhap.cs
@@ -20,7 +20,7 @@ namespace LibraryManagement
                 String connectionString = @"Data Source = (local); Initial Catalog = QuanLyThuVien;
                                         User ID = " + username + "; Password = " + password;
                 SqlConnection conn = new SqlConnection(connectionString);
-
+                conn.Open();
                 SqlCommand cmd = new SqlCommand("LAY_MA_NHAN_VIEN", conn);
                 cmd.CommandType = CommandType.Text;
                 cmd.CommandText = "SELECT [dbo].[LAY_MA_NHAN_VIEN] (@Username)";
